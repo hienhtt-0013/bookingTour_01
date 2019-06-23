01a3208572ef5782eb87f3b8002bdaa3f30ae09b
hienhtt-0013 <ho.thi.thu.hien@sun-asterisk.com>
2019-06-23 17:28:00 +0700

    add template admin

---
+++ src/main/java/app/controller/AdminRouteSampleController.java
@@ -0,0 +1,26 @@
+package app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AdminRouteSampleController {
+
+	@RequestMapping(value = {"/admin","/admin/index"}, method = RequestMethod.GET)
+	public String index() {
+		return "admin/index";
+	}
+	
+	@RequestMapping(value = "/admin/users", method = RequestMethod.GET)
+	public String users() {
+		return "admin/user/index";
+	}
+
+	@RequestMapping(value = "/admin/users/add", method = RequestMethod.GET)
+	public String usersAdd() {
+		return "admin/user/add";
+	}
+}
